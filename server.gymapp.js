ccb7a2fd8ef043779dfdeca9a8074be8df08a6bb
Nick Jennings <nick@silverbucket.net>
2014-10-02 00:05:55 +0200

    server startup script

---
+++ server.gymapp.js
@@ -0,0 +1,21 @@
+process.umask(077);
+var reStore = require('./lib/restore');
+
+// To use the Redis store:
+var store   = new reStore.Redis({
+  host:     'localhost',    // default is 'localhost'
+  port:     6379,           // default is 6379
+  database: 2,              // default is 0
+  password: ''              // default is no password
+});
+
+var server  = new reStore({
+		store:  store,
+    http:   { port: 80 },
+    allow:  {
+      signup: true,
+      debug: true
+    }
+});
+
+server.boot();
